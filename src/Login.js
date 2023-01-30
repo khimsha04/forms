112d0a27849e1587171f6a4ee050ac86b4155f04
Zurab Khimshiashvili <64226929+khimsha04@users.noreply.github.com>
2023-01-30 09:57:07 +0400

    first commit

---
+++ src/Login.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+    const validator = ()=>{
+       
+
+        if(email===""){
+                alert('enter your email')
+        }
+        if(pass===""){
+            alert('enter your password')
+    }
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="password">password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <button onClick={()=>validator()} type="submit">Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
+    )
+}
